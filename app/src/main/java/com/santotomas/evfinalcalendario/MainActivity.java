8bf4d1c84e3c27b874524a54ccfb619cdb59b501
Sarai Anais Osorio Espinaza <s.osorio15@alumnos.santotomas.cl>
2023-10-02 23:50:55 -0300

    a

--- app/src/main/java/com/santotomas/evfinalcalendario/MainActivity.java
+++ app/src/main/java/com/santotomas/evfinalcalendario/MainActivity.java
@@ -1,14 +1,40 @@
 package com.santotomas.evfinalcalendario;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main_login);
+
+        Button btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivityHome.class);
+            startActivity(intent);
+        });
+
+        Button btnregister = findViewById(R.id.btnregister);
+
+        btnregister.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivityRegister.class);
+            startActivity(intent);
+        });
     }
 }
